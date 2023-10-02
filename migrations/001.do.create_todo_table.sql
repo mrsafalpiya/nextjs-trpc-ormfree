1969c47d16f8ec44238b030cad156399ca69202d
Safal Piya <mrsafalpiya@gmail.com>
2023-10-02 14:19:58 +0545

    Added postgrator support

---
+++ migrations/001.do.create_todo_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE todo (
+	id serial PRIMARY KEY,
+	title TEXT NOT NULL,
+	info TEXT NOT NULL
+);
--- src/backend-trpc/services/todo/index.pgtypes.ts
+++ src/backend-trpc/services/todo/index.pgtypes.ts
@@ -15,3 +15,4 @@ export interface IAllTodosSqlQuery {
   params: IAllTodosSqlParams;
   result: IAllTodosSqlResult;
 }
+
